b2f8556f5c7f3c6678dd6d452d42b5e08d163848
jeongbbn <54277396+jeongbbn@users.noreply.github.com>
2020-09-27 10:08:15 +0900

    [1904] 01타일

---
+++ BOJ/1904.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int dp[1000001];
+
+int n;
+int main() {
+	scanf("%d", &n);
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2])% 15746;
+	}
+	printf("%d", dp[n]);
+
+}

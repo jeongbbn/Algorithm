5e1c23ddf3349f7cb355c06a2a0d9df0387ca2e9
jeongbbn <54277396+jeongbbn@users.noreply.github.com>
2020-11-14 15:31:39 +0900

    [9656] 돌 게임2

---
+++ BOJ/9656.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+
+int n;
+int main() {
+	scanf("%d", &n);
+	if (n % 2 == 1) printf("CY");
+	else printf("SK");
+}

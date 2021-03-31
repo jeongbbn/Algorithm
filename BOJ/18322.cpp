359753f0a015ac58059b9f077b7e24beb35fc6ae
jeongbbn <54277396+jeongbbn@users.noreply.github.com>
2021-03-31 22:07:21 +0900

    [18322] Word Processor

--- BOJ/18322.cpp
+++ BOJ/18322.cpp
@@ -1,22 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define INF 1e9
+#define mod 10007
+typedef long long ll;
+typedef pair<int, int>pi;
+typedef pair<int, pi>pii;
 
-int n, m;
-string str[105];
-
+int n, k, sum;
+vector<string>v;
 int main() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> str[i];
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		int len = str[i].length();
-		if (sum + len <= m) {
-			cout << str[i] << " "; sum += len;
+	cin >> n >> k;
+	v.resize(n);
+	for (int i = 0; i < n; i++) cin >> v[i];
+
+	cout << v[0];
+	sum = v[0].length();
+	for (int i = 1; i < n; i++)
+	{
+		if (sum + v[i].length() > k) {
+			cout << "\n" << v[i];
+			sum = v[i].length();
 		}
-		else { 
-			cout << "\n" << str[i] << " "; sum = len; 
+		else {
+			sum += v[i].length();
+			cout << " " << v[i];
 		}
 	}
 }
